55c7d37d8160800652be95fdebb5786a2397f7e8
deboradum <pvwpvw@protonmail.com>
2025-04-06 14:54:52 +0200

    finalized vqvae2 implementation, now testing

--- models/vqvae.py
+++ models/vqvae.py
@@ -10,13 +10,16 @@ class VQVAE2(nn.Module):
     def __init__(self, encoder_h_dim, res_h_dim, num_res_layers, k, d, beta):
         super().__init__()
 
-        self.encoder_top = Top_encoder(3, encoder_h_dim, res_h_dim, num_res_layers)
+        self.encoder_top = Top_encoder(
+            encoder_h_dim, encoder_h_dim, res_h_dim, num_res_layers
+        )
         self.pre_quantization_conv_top = nn.Conv2d(encoder_h_dim, d, kernel_size=1)
         self.quantizer_top = Quantizer(k, d, beta)
         self.decoder_top = Top_decoder(d, encoder_h_dim, d, res_h_dim, num_res_layers)
+        self.top_upscale = nn.ConvTranspose2d(d, d, 4, stride=2, padding=1)
 
         self.encoder_btm = Bottom_encoder(
-            encoder_h_dim, encoder_h_dim, res_h_dim, num_res_layers
+            3, encoder_h_dim, res_h_dim, num_res_layers
         )
         self.pre_quantization_conv_btm = nn.Conv2d(encoder_h_dim+d, d, kernel_size=1)
         self.quantizer_btm = Quantizer(k, d, beta)
@@ -29,23 +32,19 @@ class VQVAE2(nn.Module):
         # ... Another stack of residual blocks then further scales down the representations by a factor of two.
         h_top = self.encoder_top(h_btm)
 
-        print("h_btm", h_btm.shape)
-        print("h_top", h_top.shape)
-        exit()
-
         # Top part of fig 2a.
         h_top = self.pre_quantization_conv_top(h_top)
-        loss_term_1, loss_term_2, e_top, perplexity = self.quantizer(h_top)
+        loss_term_1, loss_term_2, e_top, perplexity = self.quantizer_top(h_top)
         dec_top = self.decoder_top(h_top)
 
         # Middle part of fig 2a.
-        h_btm = self.pre_quantization_conv_btm(mx.concatenate(h_btm, dec_top, axis=1))  # TODO: Axis correct?
-        loss_term_1, loss_term_2, e_btm, perplexity = self.quantizer(h_btm)
+        h_btm = self.pre_quantization_conv_btm(mx.concatenate((h_btm, dec_top), axis=-1))
+        loss_term_1, loss_term_2, e_btm, perplexity = self.quantizer_btm(h_btm)
 
         # Upsample e_top so it can be concatenated with e_btm before final decoder.
-        # TODO: upsample e_top
+        e_top = self.top_upscale(e_top)
 
         # The decoder ...[] takes as input all levels of the quantized latent hierarchy
-        x_hat = self.decoder(mx.concatenate(e_btm, e_top, axis=1))  # TODO: axis correct?
+        x_hat = self.decoder(mx.concatenate((e_btm, e_top), axis=-1))
 
         return x_hat, loss_term_1, loss_term_2, perplexity
